f05f9c7395c497dcc04235d681769e7e742b7f15
maymilla <164962489+MaylaYL@users.noreply.github.com>
2024-11-03 09:42:25 +0700

    fix: schedule atletik, badminton

--- app/Schedule/Estafet/schedule.ts
+++ app/Schedule/Estafet/schedule.ts
@@ -5,6 +5,7 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 1",
+                image1: "./IMMG.png",
                 skor1: " ",
                 tim2: "Ikatan Mahasiswa Metalurgi",
                 image2: "./IMMG.png",
@@ -13,6 +14,7 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 1",
+                image1: "./MTI.png",
                 skor1: " ",
                 tim2: "Keluarga Mahasiswa Teknik Industri",
                 image2: "./MTI.png",
@@ -21,14 +23,16 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 1",
+                image1: "./HMF.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Farmasi 'Ars Praeparandi'",
-                image2: "./HMF AP.png",
+                image2: "./HMF.png",
                 skor2: " "
             },
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 1",
+                image1: "./MTM.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Teknik Material",
                 image2: "./MTM.png",
@@ -37,22 +41,25 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 1",
+                image1: "./HIMAMIKRO.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Mikrobiologi 'Archaea'",
-                image2: "./HIMAMIKRO Archaea.png",
+                image2: "./HIMAMIKRO.png",
                 skor2: " "
             },
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 2",
+                image1: "./HMME.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Meteorologi 'Atmosphaira'",
-                image2: "./HMME Atmosphaira.png",
+                image2: "./HMME.png",
                 skor2: " "
             },
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 2",
+                image1: "./IMG.png",
                 skor1: " ",
                 tim2: "Ikatan Mahasiswa Geodesi",
                 image2: "./IMG.png",
@@ -61,30 +68,34 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 2",
+                image1: "./HMO.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Oseanografi",
-                image2: "./HMO TRITON.png",
+                image2: "./HMO.png",
                 skor2: " "
             },
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 2",
+                image1: "./HMTM.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Teknik Perminyakan 'PATRA'",
-                image2: "./HMTM PATRA.png",
+                image2: "./HMTM.png",
                 skor2: " "
             },
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 2",
+                image1: "./IMT.png",
                 skor1: " ",
                 tim2: "Ikatan Mahasiswa Telekomunikasi 'Signum'",
-                image2: "./IMT Signum.png",
+                image2: "./IMT.png",
                 skor2: " "
             },
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 3",
+                image1: "./KMPN.png",
                 skor1: " ",
                 tim2: "Keluarga Mahasiswa Teknik Penerbangan",
                 image2: "./KMPN.png",
@@ -93,6 +104,7 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 3",
+                image1: "./HMS.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Sipil",
                 image2: "./HMS.png",
@@ -101,14 +113,16 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 3",
+                image1: "./HIMA_TG.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Teknik Geofisika 'TERRA'",
-                image2: "./HIMA TG TERRA.png",
+                image2: "./HIMA_TG.png",
                 skor2: " "
             },
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 3",
+                image1: "./HIMASTRON.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Astronomi",
                 image2: "./HIMASTRON.png",
@@ -117,6 +131,7 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 3",
+                image1: "./HIMATIKA.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Matematika",
                 image2: "./HIMATIKA.png",
@@ -125,6 +140,7 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 4",
+                image1: "./HIMATEK.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Teknik Kimia",
                 image2: "./HIMATEK.png",
@@ -133,6 +149,7 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 4",
+                image1: "./HMT.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Tambang",
                 image2: "./HMT.png",
@@ -141,6 +158,7 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 4",
+                image1: "./HME.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Elektroteknik",
                 image2: "./HME.png",
@@ -149,6 +167,7 @@ export const schedule = [
             {
                 sportName: "Estafet Grup",
                 tim1: "Grup 4",
+                image1: "./HMTL.png",
                 skor1: " ",
                 tim2: "Himpunan Mahasiswa Teknik Lingkungan",
                 image2: "./HMTL.png",
--- app/Schedule/Sprint Putri/schedule.ts
+++ app/Schedule/Sprint Putri/schedule.ts
@@ -6,22 +6,25 @@ export const schedule = [
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 1",
+                    image1: "./HMY.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Tambang",
+                    tim2: "Himpunan Mahasiswa Tambang (B)",
                     image2: "./HMT.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 1",
+                    image1: "./MTM.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Teknik Material",
+                    tim2: "Himpunan Mahasiswa Teknik Material (A)",
                     image2: "./MTM.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 1",
+                    image1: "./IMG.png",
                     skor1: " ",
                     tim2: "Ikatan Mahasiswa Geodesi",
                     image2: "./IMG.png",
@@ -30,6 +33,7 @@ export const schedule = [
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 1",
+                    image1: "./KMSR.png",
                     skor1: " ",
                     tim2: "Keluarga Mahasiswa Seni Rupa",
                     image2: "./KMSR.png",
@@ -38,6 +42,7 @@ export const schedule = [
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 2",
+                    image1: "./HMIF.png",
                     skor1: " ",
                     tim2: "Himpunan Mahasiswa Informatika",
                     image2: "./HMIF.png",
@@ -46,118 +51,133 @@ export const schedule = [
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 2",
+                    image1: "./HMO.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Oseanografi",
-                    image2: "./HMO TRITON.png",
+                    tim2: "Himpunan Mahasiswa Oseanografi (A)",
+                    image2: "./HMO.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 2",
+                    image1: "./HMTL.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Teknik Lingkungan",
+                    tim2: "Himpunan Mahasiswa Teknik Lingkungan (B)",
                     image2: "./HMTL.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 2",
+                    image1: "./MTM.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Teknik Material",
+                    tim2: "Himpunan Mahasiswa Teknik Material (B)",
                     image2: "./MTM.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 3",
+                    image1: "./HMTL.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Teknik Lingkungan",
+                    tim2: "Himpunan Mahasiswa Teknik Lingkungan(A)",
                     image2: "./HMTL.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 3",
+                    image1: "./HMS.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Sipil",
+                    tim2: "Himpunan Mahasiswa Sipil (A)",
                     image2: "./HMS.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 3",
+                    image1: "./HMT.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Tambang",
+                    tim2: "Himpunan Mahasiswa Tambang (A)",
                     image2: "./HMT.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 4",
+                    image1: "./HMS.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Sipil",
+                    tim2: "Himpunan Mahasiswa Sipil (B)",
                     image2: "./HMS.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 4",
+                    image1: "./IMMG.png",
                     skor1: " ",
-                    tim2: "Ikatan Mahasiswa Metalurgi",
+                    tim2: "Ikatan Mahasiswa Metalurgi (B)",
                     image2: "./IMMG.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 4",
+                    image1: "./KMPN.png",
                     skor1: " ",
-                    tim2: "Keluarga Mahasiswa Teknik Penerbangan",
+                    tim2: "Keluarga Mahasiswa Teknik Penerbangan (B)",
                     image2: "./KMPN.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 5",
+                    image1: "./HMO.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Oseanografi",
-                    image2: "./HMO TRITON.png",
+                    tim2: "Himpunan Mahasiswa Oseanografi (B)",
+                    image2: "./HMO.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 5",
+                    image1: "./KMPN.png",
                     skor1: " ",
-                    tim2: "Keluarga Mahasiswa Teknik Penerbangan",
+                    tim2: "Keluarga Mahasiswa Teknik Penerbangan (A)",
                     image2: "./KMPN.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 5",
+                    image1: "./HMFT.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Fisika Teknik",
+                    tim2: "Himpunan Mahasiswa Fisika Teknik (A)",
                     image2: "./HMFT.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 6",
+                    image1: "./HMF.png",
                     skor1: " ",
                     tim2: "Himpunan Mahasiswa Farmasi 'Ars Praeparandi'",
-                    image2: "./HMF AP.png",
+                    image2: "./HMF.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 6",
+                    image1: "./HMFT.png",
                     skor1: " ",
-                    tim2: "Himpunan Mahasiswa Fisika Teknik",
+                    tim2: "Himpunan Mahasiswa Fisika Teknik (B)",
                     image2: "./HMFT.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 6",
+                    image1: "./HIMATIKA.png",
                     skor1: " ",
                     tim2: "Himpunan Mahasiswa Matematika",
                     image2: "./HIMATIKA.png",
@@ -166,14 +186,16 @@ export const schedule = [
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 7",
+                    image1: "./IMMG.png",
                     skor1: " ",
-                    tim2: "Ikatan Mahasiswa Metalurgi",
+                    tim2: "Ikatan Mahasiswa Metalurgi (A)",
                     image2: "./IMMG.png",
                     skor2: " "
                 },
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 7",
+                    image1: "./MTI.png",
                     skor1: " ",
                     tim2: "Keluarga Mahasiswa Teknik Industri",
                     image2: "./MTI.png",
@@ -182,9 +204,10 @@ export const schedule = [
                 {
                     sportName: "Atletik Putri",
                     tim1: "Grup 7",
+                    image1: "./HIMABIO.png",
                     skor1: " ",
                     tim2: "Himpunan Mahasiswa Biologi",
-                    image2: "./HIMABIO Nymphaea.png",
+                    image2: "./HIMABIO.png",
                     skor2: " "
                 }
             ]
